503411d052fa1fdb4afe0eabdbe8c30eff9cf06a
HanseongBae <bha4388@naver.com>
2023-08-12 21:54:25 +0900

    section17 : 객체

---
+++ section17/Object.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    # Object : 데이터 와 함수, 상태와 행위를 가지는 객체
+    # 파이썬에서는 대부분 모든것이 객체다.
+
+    #List 객체 상태
+    my_list = [1, 2, 3]
+
+    #List 객체의 행동, 행위
+    my_list.append(4)
+    print(my_list)
